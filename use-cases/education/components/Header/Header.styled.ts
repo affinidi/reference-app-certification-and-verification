a7906031c080f5930ee6a954efdbab7ebc90825b
Lloyd Grogan <lloydgrogan@affinidi.com>
2023-02-17 17:52:51 +0100

    feat: add dynamic margin/padding props for each project on home screen

--- use-cases/education/components/Header/Header.styled.ts
+++ use-cases/education/components/Header/Header.styled.ts
@@ -14,12 +14,16 @@ export const IconWrapper = styled.div`
   }
 `
 
-export const Container = styled(Box)`
+export const Container = styled(Box)<{
+  $isProject?: "education" | "health" | "ticketing"
+}>`
   height: ${pxToRem(144)};
   padding: 0 ${pxToRem(100)};
   background-color: ${(props) => props.theme.colors.brand.primary['90']};
+  margin-bottom: ${(props) => props.$isProject === "education" ? pxToRem(48) : pxToRem(24)};}
 
   @media (max-width: 1024px) {
+    margin-bottom: ${(props) => props.$isProject === "education" ? pxToRem(48) : pxToRem(16)};}
     padding: 0 ${pxToRem(24)};
     height: ${pxToRem(164)};
   }
--- use-cases/ticketing/components/Header/Header.styled.ts
+++ use-cases/ticketing/components/Header/Header.styled.ts
@@ -14,10 +14,14 @@ export const IconWrapper = styled.div`
   }
 `
 
-export const Container = styled(Box)`
+export const Container = styled(Box)<{
+  $isProject?: "education" | "health" | "ticketing"
+}>`
   height: ${pxToRem(144)};
+  margin-bottom: ${(props) => props.$isProject === "education" ? pxToRem(48) : pxToRem(24)};}
 
   @media (max-width: 1024px) {
+    margin-bottom: ${(props) => props.$isProject === "education" ? pxToRem(48) : pxToRem(16)};}
     height: ${pxToRem(164)};
   }
 `
